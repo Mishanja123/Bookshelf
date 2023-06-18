32015266916c05dd6d6c4ad40deaab702bc348c7
SweetLanaMay <121062437+SweetLanaMay@users.noreply.github.com>
2023-06-18 13:14:13 +0300

    modal 2 html+css

--- src/js/modals.js
+++ src/js/modals.js
@@ -1,11 +1,40 @@
-function fetchBook() {
-  const url = 'https://books-backend.p.goit.global/books/{642fd89ac8cf5ee957f12361}';
+import { fetchBook } from './fetchapis.js';
 
-  return fetch(url).then(responce => {
-    if (!responce.ok) {
-      throw new Error(responce.status);
-    }
-    console.log(responce);
-    return responce.json();
+const modalContent = document.querySelector('.modal-content');
+
+export async function markBooItem() {
+  const markup = await fetchBook().then(resp => {
+    console.log(resp)
+    return resp
+      .map(({ book_image, title, author, description, _id }) => {
+        return `
+        <div class="book" data-book-id="${_id}">
+        <img class="modal-image" src="${book_image}" alt="Book cover" />
+          <h3 class="modal-title">${title}</h3>
+          <p class="modal-author">${author}</p>
+          <p class="modal-description">${description}</p>
+        </div>`;
+      })
+      .join('');
   });
+  console.log(markup);
+  return markup;
 }
+
+async function createBookItem(book) {
+  const mark = await markBooItem(book);
+  modalContent.innerHTML = mark;
+}
+
+modalContent.addEventListener('click', onClick);
+
+async function onClick(e) {
+  e.preventDefault();
+  const target = e.target;
+  
+  if (target.classList.contains('book')) {
+  const bookId = target.dataset.bookId;
+  const book = await fetchBook(bookId);
+  createBookItem(book);
+  }
+  }
