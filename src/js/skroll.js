5ecff1e4c2fe56129418855657f182359ac03c1e
soberjan1 <aliguseynov48@gmail.com>
2023-06-18 20:32:44 +0300

    first create header

--- src/js/skroll.js
+++ src/js/skroll.js
@@ -1,19 +1,18 @@
-let lastScroll = 0;
-const defaultOffset = 100;
-const header = document.querySelector('.header');
+// let lastScroll = 0;
+// const defaultOffset = 30;
+// const header = document.querySelector('.header');
 
-const scrollPosition = () => window.pageYOffset || document.documentElement.scrollTop;
-const containHide = () => header.classList.contains('hide');
-window.addEventListener('scroll', () => {
-    if (scrollPosition() > lastScroll && !containHide() && scrollPosition() > defaultOffset) {
-        header.classList.add('hide')
-        //scroll down
-    }
-    else if (scrollPosition() < lastScroll && containHide()) {
-        header.classList.add('hide')
-        //scroll up
-    }
-    
-    
-    lastScroll = scrollPosition();
-} )
+// const scrollPosition = () =>
+//   window.pageYOffset || document.documentElement.scrollTop;
+// const containHide = () => header.classList.contains('hidden');
+// window.addEventListener('scroll', () => {
+//   if (scrollPosition() > lastScroll && scrollPosition() > defaultOffset) {
+//     header.classList.add('hidden');
+//     //scroll down
+//   } else if (scrollPosition() < lastScroll && containHide()) {
+//     header.classList.remove('hidden');
+//     //scroll up
+//   }
+
+//   lastScroll = scrollPosition();
+// });
