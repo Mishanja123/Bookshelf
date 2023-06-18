76bc1ef0d21356fb5651d05b78a8ecb9efe3c442
Zhanna93 <121199582+Zhanna93@users.noreply.github.com>
2023-06-18 23:15:59 +0300

    slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,16 +1,27 @@
-//  import Swiper, { Navigation, Pagination } from 'swiper';
-// // import Swiper and modules styles
-// import '../../node_modules/swiper/swiper-bundle.css';
-  // import '../../node_modules/swiper/swiper.css';
-  // import '../../node_modules/swiper/modules/navigation';
-  // import '../../node_modules/swiper/modules/pagination';
-
-import slick from "../../node_modules/slick-slider";
-// import jQuery from "../../node_modules/jquery";
+import Swiper, { Navigation, Pagination } from 'swiper';
+import '../../node_modules/swiper/swiper.css';
 
 const sliderBtn = document.querySelector('.slider-btn');
 
-// $(document).ready(function () {
-//  $('.slider').slick();
-// });
+// sliderBtn.addEventListener('click', () => {
+ 
+// })
 
+ const mySwiperThree = new Swiper('.swiper', {
+  direction: 'vertical',
+  autoHeight: true,
+  slidesPerView: 6,
+  watchOverflow: true,
+  // spaceBetween: 20,
+  slidesPerGroup: 1,
+  freeMode: true,
+  speed: 800,
+  scrollbar: {
+   el: '.swiper-scrollbar',
+   draggable: true,
+  },
+  mousewheel: {
+   sensitivity: 1,
+   eventsTarget: '.swiper-slide'
+  }
+ });
