857a0c3a4cca758c90ac3640be820838baa88682
EdHloba <hloba.e.v@gmail.com>
2023-06-19 16:22:16 +0300

    Create rendershoppinglist.js

---
+++ src/js/rendershoppinglist.js
@@ -0,0 +1,83 @@
+const refs = {
+    articlesContainer: document.querySelector('shopping-list'),
+
+}
+
+function appendArticleMarkup(books) {
+  refs.articlesContainer.insertAdjacentHTML('beforeend', createMarkup(books));
+}
+
+function createMarkup(books) {
+  const markup = books
+    .map(
+      book =>
+        `<li class="shopping-item">
+            <div class="shopping-card-thumb">
+                <img
+                    src="${book.image}"
+                    width=""
+                    height=""
+                    alt=""
+                    class="book-img"
+                />
+            </div>
+            <div class="shopping-card-content">
+                <h2 class="book-title">${book.title}</h2>
+                <p class="book-category">${book.category}</p>
+                <p class="book-description cut-text">
+                    ${book.description}
+                </p>
+                <p class="book-author">${book.author}</p>
+                <ul class="buy-links-list list">
+                    <li class="buy-links-item">
+                         <a
+                            href="${book.buy-links[0]}"
+                            class="buy-link"
+                        >
+                            <img
+                                src="/src/images/modal-images/image1.png"
+                                width=""
+                                height=""
+                                alt=""
+                            />
+                         </a>
+                    </li>
+                    <li class="buy-links-item">
+                        <a
+                         href="${book.buy-links[1]}"
+                            class="buy-link"
+                         >
+                            <img
+                            src="/src/images/modal-images/image2.png"
+                            width=""
+                            height=""
+                            alt=""
+                            />
+                        </a>
+                    </li>
+                    <li class="buy-links-item">
+                         <a
+                            href="${book.buy-links[2]}"
+                            class="buy-link"
+                        >
+                            <img
+                            src="/src/images/modal-images/image3.png"
+                            width=""
+                            height=""
+                            alt=""
+                            />
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <button class="delete-button" type="button">
+                <svg class="icon" width="16" height="16">
+                    <use href="/src/images/sprite.svg#icon-delete"></use>
+                </svg>
+            </button>
+        </li>                 
+        `
+    )
+    .join('');
+  return markup;
+}
