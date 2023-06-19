efeae24d36de7a246184faef645fa7ea897c9ff1
Misha <shevchenkom606@gmail.com>
2023-06-19 21:56:38 +0200

    create the support block

---
+++ src/js/data-from-support.js
@@ -0,0 +1,60 @@
+export const support_array = [
+  {
+    title: 'Save the Children',
+    url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
+    img: require('../images/support-block/image1.png'),
+    id: '01',
+  },
+  {
+    title: 'Project HOPE',
+    url: 'https://www.projecthope.org/country/ukraine/',
+    img: require('../images/support-block/image2.png'),
+
+    id: '02',
+  },
+  {
+    title: 'International Medical Corps',
+    url: 'https://internationalmedicalcorps.org/country/ukraine/',
+    img: require('../images/support-block/image4.png'),
+    id: '03',
+  },
+  {
+    title: 'RAZOM',
+    url: 'https://www.razomforukraine.org/',
+    img: require('../images/support-block/image6.png'),
+
+    id: '04',
+  },
+  {
+    title: 'Action against hunger',
+    url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
+    img: require('../images/support-block/image7.png'),
+    id: '05',
+  },
+  {
+    title: 'Serhiy Prytula Charity Foundation',
+    url: 'https://prytulafoundation.org/en',
+    img: require('../images/support-block/image9.png'),
+    id: '06',
+  },
+  {
+    title: 'UNITED24',
+    url: 'https://u24.gov.ua/uk',
+    img: require('../images/support-block/image3.png'),
+    id: '07',
+  },
+
+  {
+    title: 'Medicins Sans Frontieres',
+    url: 'https://www.msf.org/ukraine',
+    img: require('../images/support-block/image5.png'),
+    id: '08',
+  },
+
+  {
+    title: 'World vision',
+    url: 'https://www.wvi.org/emergencies/ukraine',
+    img: require('../images/support-block/image8.png'),
+    id: '09',
+  },
+];
