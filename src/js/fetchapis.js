93b4b32a33bff9a5321a87e4889262807661c442
Trifonov Denys <trifonowden123@gmail.com>
2023-06-21 01:05:29 +0300

    notifications

--- src/js/fetchapis.js
+++ src/js/fetchapis.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-
+import Notiflix from 'notiflix';
 let targeturl = '';
 const options = {
   method: 'get',
@@ -9,24 +9,36 @@ const options = {
 
 export async function fetchCategoryList() {
   options.url = 'category-list';
-  const test = await axios(options).then(rep => rep.data);
+  const test = await axios(options).then(rep => rep.data).catch(error => {
+    console.log(error.message);
+    Notiflix.Notify.failure('Bed request')
+  } );
   return test;
 }
 
 export async function fetchCategory(target) {
   options.url = `category?category=${target}`;
-  const test = await axios(options).then(rep => rep.data);
+  const test = await axios(options).then(rep => rep.data).catch(error => {
+    console.log(error.message);
+    Notiflix.Notify.failure('Bed request')
+  } );
   return test;
 }
 
 export async function fetchBestSellers() {
   options.url = 'top-books';
-  const test = await axios(options).then(rep => rep.data);
+  const test = await axios(options).then(rep => rep.data).catch(error => {
+    console.log(error.message);
+    Notiflix.Notify.failure('Bed request')
+  } );
   return test;
 }
 
 export async function fetchBook(bookId) {
   options.url = `${bookId}`;
-  const test = await axios(options).then(rep => rep.data);
+  const test = await axios(options).then(rep => rep.data).catch(error => {
+    console.log(error.message);
+    Notiflix.Notify.failure('Bed request')
+  } );
   return test;
 }
