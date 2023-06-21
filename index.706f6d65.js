21ae71f6b4b29c1fc5a07e97dd939128cf110394
Mishanja123 <116510947+Mishanja123@users.noreply.github.com>
2023-06-21 17:18:49 +0000

    Deploying to gh-pages from @ Mishanja123/js-project-team-7@1a919a8434c4727fc2d71e2a7b748a61241422b6 🚀

---
+++ index.706f6d65.js
@@ -0,0 +1,2 @@
+!function(){var o=document.querySelector(".scroll-up");window.onscroll=function(){document.body.scrollTop>500||document.documentElement.scrollTop>500?(o.style.display="block",o.classList.add("show")):(o.style.display="none",o.classList.remove("show"))},o.addEventListener("click",(function(){window.scrollTo({top:0,behavior:"smooth"})}))}();
+//# sourceMappingURL=index.706f6d65.js.map
--- index.8c16116e.js
+++
@@ -1,2 +0,0 @@
-let o=document.querySelector(".scroll-up");window.onscroll=function(){document.body.scrollTop>500||document.documentElement.scrollTop>500?(o.style.display="block",o.classList.add("show")):(o.style.display="none",o.classList.remove("show"))},o.addEventListener("click",(function(){document.body.scrollTop=0,document.documentElement.scrollTop=0}));
-//# sourceMappingURL=index.8c16116e.js.map
