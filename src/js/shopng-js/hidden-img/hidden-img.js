1e51fda2c8ead619bf3031906cc494bf46236ccc
JoggerChe <120989445+BorovChe@users.noreply.github.com>
2023-06-22 04:29:20 +0300

    push

---
+++ src/js/shopng-js/hidden-img/hidden-img.js
@@ -0,0 +1,8 @@
+import { refs } from '../DOM-refs/refs-DOM';
+export function showImg(arr) {
+  if (arr.length > 0) {
+    refs.descriptionBlock.classList.add('visually-hidden');
+  } else {
+    refs.descriptionBlock.classList.remove('visually-hidden');
+  }
+}
